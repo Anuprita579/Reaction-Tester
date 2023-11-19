d148d48e546a10c8b545a0c894b7648cd9d9cd9b
Anuprita <anupritamhapankar22@gmail.com>
2023-11-19 17:52:26 +0530

    initial commit

---
+++ rt.js
@@ -0,0 +1,29 @@
+var start = new Date().getTime();
+        function getRandomColor(){
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for(var i=0; i<6; i++){
+                color += letters[Math.floor(Math.random()*16)];
+            }
+            return color;
+        }
+        function move(){
+            var left=Math.random()*300;
+            var top=Math.random()*300;
+            var wh=((Math.random()*500) + 100);
+            document.getElementById("disbox").style.left = left + 'px';
+            document.getElementById("disbox").style.top = top + 'px';
+            document.getElementById("disbox").style.width = wh + 'px';
+            document.getElementById("disbox").style.height = wh + 'px';
+            document.getElementById("disbox").style.display = 'block';
+            document.getElementById("disbox").style.backgroundColor=getRandomColor();
+            start = new Date().getTime();
+        }
+        move();
+        document.getElementById("disbox").onclick = function(){
+            document.getElementById("disbox").style.display = 'none';
+            var end = new Date().getTime();
+            var x = (end - start)/1000;
+            alert("Time taken is :"+ x);
+            move();
+        }
